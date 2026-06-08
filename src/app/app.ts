ea2a9f4713e64ea8fbabf8ce3708ddb74199eeeb
donavangrobler <donavangrobler2@gmail.com>
2026-06-08 11:22:54 +0200

    Uses input and output signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
-import { User } from "./user/user";
+import { User } from './user/user';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   imports: [Header, User],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-the-complete-guide');
+  users = signal(DUMMY_USERS);
+
+  onSelectUser(id: string) {
+    console.log('Selected user ID:', id);
+  }
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import {DUMMY_USERS} from "../dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, computed, EventEmitter, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,7 +7,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  // @Output() select = new EventEmitter();
 
-  selectedUser = DUMMY_USERS[randomIndex];
+  id = input.required<string>();
+  avatar = input.required<string>();
+  name = input.required<string>();
+  select = output<string>();
 
+  imagePath = computed(() => `assets/users/${this.avatar()}`);
+
+  onSelectUser() {
+    this.select.emit(this.id());
+  }
 }
