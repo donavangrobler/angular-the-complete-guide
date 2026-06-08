48d0711f76be0e63163c9458ea12c1559186a857
donavangrobler <donavangrobler2@gmail.com>
2026-06-08 17:00:53 +0200

    Use date pipe to display date better

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,5 +1,6 @@
 import { Component, input, output } from '@angular/core';
-import { Card } from "../../shared/card/card";
+import { Card } from '../../shared/card/card';
+import { DatePipe } from '@angular/common';
 
 interface TaskType {
   id: string;
@@ -11,7 +12,7 @@ interface TaskType {
 
 @Component({
   selector: 'app-task',
-  imports: [Card],
+  imports: [Card, DatePipe],
   templateUrl: './task.html',
   styleUrl: './task.css',
 })
