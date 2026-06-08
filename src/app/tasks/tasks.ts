71a088b88e5e47e6f4e0c3da1cc53c176668b709
donavangrobler <donavangrobler2@gmail.com>
2026-06-08 14:29:09 +0200

    Adds individual tasks

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,11 +1,38 @@
-import { Component, input } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [Task],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
 export class Tasks {
   name = input.required<string | undefined>();
+  userId = input.required<string | undefined>();
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary: 'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
+
+  selectUserTasks = computed(() => this.tasks.filter((task) => task.userId === this.userId()));
 }
